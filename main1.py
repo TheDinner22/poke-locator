127566c1c8a1f825d4d994e1b728533e9e5df08e
joe <not_real_email@gmail.com>
2021-09-27 09:09:55 -0400

    made main i guess???

---
+++ main1.py
@@ -0,0 +1,58 @@
+# dependencies
+import os, sys, time, datetime, cv2
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # adds project dir to places it looks for the modules
+sys.path.append(BASE_PATH)
+
+from test.main_test import _App
+from lib.screenshot import Camera, on_click
+from lib.proccess_img import Processor
+from lib.proccess_text import Text_Processor
+from lib.loud import Speaker
+
+running = True
+print('type "help" for help')
+while running:
+    user_input = input('$').strip().lower()
+    if user_input == 'kill':
+        running = False
+    elif user_input == 'test':
+        # run tester
+        tester = _App()
+        tester.run_all_tests()
+    elif user_input == 'help':
+        print('"kill" - kill the program')
+        print('"test" - run all unit and integration test')
+        print('"start" - starts the loop to search for pokemon')
+    elif user_input == 'start':
+        # set the screen shot zone
+        camera = Camera(on_click)
+        camera.start_mouse_listener()
+
+        # loop
+        flag = True
+        while flag:
+
+            # take screen shot
+            camera.take_screenshot()
+
+            # get text
+            processor = Processor('.data/images/screenshot.png')
+            processor.turn_black_and_white()
+            processor.image_to_txt()
+
+            text = processor.text_from_img
+
+            # check text
+            text_processor = Text_Processor(text)
+            text_processor.proccess_text()
+
+            # alert and save the ss if found
+            if text_processor.pokemon_spawned:
+                # alert the user
+                speaker = Speaker(3)
+                speaker.play()
+
+            # wait 
+            time.sleep(2)
+
